f0037999177673ce6657df304c9ddf95779fe217
David Nussio <david.nussio@gmail.com>
2020-12-10 23:23:38 +0100

    Declare namespace

--- index.d.ts
+++ index.d.ts
@@ -1,37 +1,41 @@
-type OpenapiSchemaContect = { name?: string; url?: string; email?: string };
+declare namespace MoleculerSwagger {
+  type OpenapiSchemaContect = { name?: string; url?: string; email?: string };
 
-type OpenapiSchemaLicense = { name: string; url?: string };
+  type OpenapiSchemaLicense = { name: string; url?: string };
 
-type OpenapiSchemaServers = {
-  url: string;
-  description?: string;
-  variables?: Array<any>;
-};
+  type OpenapiSchemaServers = {
+    url: string;
+    description?: string;
+    variables?: Array<any>;
+  };
 
-type OpenapiSchemaInfo = {
-  title: string;
-  description?: string;
-  termsOfService?: string;
-  contact?: OpenapiSchemaContect;
-  license?: OpenapiSchemaLicense;
-  version: string;
-};
+  type OpenapiSchemaInfo = {
+    title: string;
+    description?: string;
+    termsOfService?: string;
+    contact?: OpenapiSchemaContect;
+    license?: OpenapiSchemaLicense;
+    version: string;
+  };
 
-type OpenapiVersion = "3.0.3";
+  type OpenapiVersion = "3.0.3";
 
-interface OpenapiSchema {
-  openapi: OpenapiVersion;
-  info: OpenapiSchemaInfo;
-  servers?: Array<OpenapiSchemaServers>;
-  paths?: any;
-  components?: any;
-  security?: any;
-  tags?: any;
-  externalDocs?: any;
+  interface OpenapiSchema {
+    openapi: OpenapiVersion;
+    info: OpenapiSchemaInfo;
+    servers?: Array<OpenapiSchemaServers>;
+    paths?: any;
+    components?: any;
+    security?: any;
+    tags?: any;
+    externalDocs?: any;
+  }
+
+  interface SwaggerSettings {
+    openapiSchema: OpenapiSchema;
+    disabled?: boolean;
+    defaults?: any;
+  }
 }
 
-interface SwaggerSettings {
-  openapiSchema: OpenapiSchema;
-  disabled?: boolean;
-  defaults?: any;
-}
+export = MoleculerSwagger;
